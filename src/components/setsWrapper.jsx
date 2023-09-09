d736fefae6bf8577344c0d83af79e054836a3746
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-09-09 14:20:31 +0200

    [+]

--- src/components/setsWrapper.jsx
+++ src/components/setsWrapper.jsx
@@ -8,7 +8,8 @@ function SetsWrapper(props) {
   return (
     <>
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 gap-5">
-            {sets.map((set) => {
+            { // eslint-disable-next-line
+            sets.map((set) => {
                 try {
                     return (
                         <Card key={set.id} setName={set.name} imgSrc={require(`../assets/logoSet/` + set.id.replace(/\s/g, '') + `.png`)}/>
